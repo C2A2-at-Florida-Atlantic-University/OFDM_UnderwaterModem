cbcff46858a3b091d52418224ea0bc119f6ab7a4
hermansjared@gmail.com <hermansjared@gmail.com>
2023-03-07 08:41:20 -0500

    Update

--- Xilinx/Vivado/build/build_radio_top_pynq.tcl
+++ Xilinx/Vivado/build/build_radio_top_pynq.tcl
@@ -32,12 +32,12 @@ cd $FAU_OFDM_REPO_PATH/Xilinx/Vivado/constraints
 add_files -fileset constrs_1 -norecurse base_pynq_z2.xdc
 cd $FAU_OFDM_REPO_PATH/Xilinx/Vivado/build/radio_top_pynq
 
-if (0) {
+if (1) {
   launch_runs synth_1 -jobs 12
   wait_on_run synth_1
   launch_runs impl_1 -to_step write_bitstream
   wait_on_run impl_1
-  write_hw_platcorm -fixed -include_bit -force -file radio_top_pynq.xsa
+  write_hw_platform -fixed -include_bit -force -file radio_top_pynq.xsa
 }
 
 start_gui
